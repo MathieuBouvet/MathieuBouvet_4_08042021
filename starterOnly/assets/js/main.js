b7185f46ab051f572e612a8c287f7644699f64d2
Mathieu Bouvet <mathieubouvet42@gmail.com>
2021-04-16 16:54:53 +0200

    Add a tiny module loader emulation

--- starterOnly/assets/js/main.js
+++ starterOnly/assets/js/main.js
@@ -1,3 +1,29 @@
-responsiveMenuToggler();
-signupModal();
-signupFormValidation();
+// Take a function name and return a Promise that resolves
+// with the coresponding function when it becomes available in global scope,
+// but rejects after 1 second
+function lookupFunction(functionName) {
+  return new Promise((resolve, reject) => {
+    const lookup = setInterval(() => {
+      if (window[functionName] != null) {
+        resolve(window[functionName]);
+        clearInterval(lookup);
+        clearTimeout(timeout);
+      }
+    });
+    const timeout = setTimeout(() => {
+      reject(new Error(`Registration timed out for function '${functionName}'`));
+      clearInterval(lookup);
+    }, 1000);
+  });
+}
+
+// Execute the function when it becomes available in the global scope
+function registerFunction(functionName) {
+  lookupFunction(functionName)
+    .then(fn => fn())
+    .catch(console.error);
+}
+
+registerFunction("responsiveMenuToggler");
+registerFunction("signupModal");
+registerFunction("signupFormValidation");
